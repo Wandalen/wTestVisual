c8c21b233ac43214c12dd0509030831176ea99e8
volodymyrmigdal <volodymyr.migdal@gmail.com>
2021-12-07 10:06:01 +0200

    init

---
+++ proto/wtools/atop/testing/entry/Main.s
@@ -0,0 +1,19 @@
+( function _Main_s_()
+{
+
+'use strict';
+
+/**
+ *Framework for convenient visual testing. Extends module Testing.
+ * @module Tools/top/testvisual
+ */
+
+if( typeof module !== 'undefined' )
+{
+  require( '../include/Top.s' );
+}
+
+if( typeof module !== 'undefined' )
+module[ 'exports' ] = _globals_.wTools;
+
+})();
---
+++ proto/wtools/atop/testing/include/Top.s
@@ -0,0 +1,21 @@
+( function _IncludeTop_s_()
+{
+
+'use strict';
+
+//
+
+if( typeof module !== 'undefined' )
+{
+  const _ = _global_.wTools;
+
+  //
+
+  require( './Base.s' );
+
+}
+
+if( typeof module !== 'undefined' )
+module[ 'exports' ] = _global_.wTools;
+
+})();
